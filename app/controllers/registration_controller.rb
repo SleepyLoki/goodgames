4480bc508d2e74d4879eedec2032ffa0fb9f511b
Enrique Cerna <ecerna96@gmail.com>
2019-09-18 22:55:17 +0000

    deleted all currently enrolled users, added 3 new fields to the users controller (bio, first name, last name), fixed alerts bug where the alert would not show up whenever user typed something wrong

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  # Modified Devise params for user login
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :bio, :email, :password, :password_confirmation)
+  end
+end
