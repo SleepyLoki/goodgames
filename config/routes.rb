b71e15bb3ccbe1c1b99ffece6260ec626db4f901
Enrique Cerna <ecerna96@gmail.com>
2019-10-20 21:31:17 +0000

    removed route for user accounts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resource :dashboard, only: [:show]
-  resource :user_accounts
   resource :my_games, only: [:show]
   resources :games do
     resources :cps, only: [:create, :destroy]
