0c7c66067a5573c23da15b04c9531a9db9f74c95
Enrique Cerna <ecerna96@gmail.com>
2019-10-21 21:55:24 +0000

    modified the registration controller to implement a 'create' action

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,5 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(sign_up_params)
+    redirect_to root_path
+  end
+
   private
 
   # Modified Devise params for user login
