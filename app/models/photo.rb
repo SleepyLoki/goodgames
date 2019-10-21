dcf2163150ff21c5906d91279f160b1987fe6e14
Enrique Cerna <ecerna96@gmail.com>
2019-10-21 23:13:28 +0000

    deleted the two migrations that were redundant. Also fixed the feture image to show everytime a game is listed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,35 @@ class Photo < ApplicationRecord
   belongs_to :game
   belongs_to :user  
   mount_uploader :picture, PictureUploader
+  mount_uploader :feature_Photo, PictureUploader
+
+  validates :feature_Photo, presence: true
+
+  def feature_limit?(game)
+    features_in_game = []
+    game.photos.each do |photo|
+      features_in_game << photo.feature_Photo
+    end
+
+    if features_in_game.length >= 1
+      return true
+    else
+      return false
+    end
+  end
+
+  def picture_added?(game)
+    pics_in_game = []
+    game.photos.each do |photo|
+      pics_in_game << photo.picture
+    end
+
+    if pics_in_game.length >= 1
+      return true
+    else
+      return false
+    end
+  end
 
   def picture_limit?(game)
     pictures_in_game = []
--- db/migrate/20191014163757_add_user_id_to_photo.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToPhoto < ActiveRecord::Migration[5.2]
-  def change
-    add_column :photos, :user_id, :integer
-  end
-
-end
