730c4a47e56a2bacadf7c91b67804b4fedc6d409
Enrique Cerna <ecerna96@gmail.com>
2019-10-23 21:51:27 +0000

    Youtube API functions corrently!

--- app/models/game.rb
+++ app/models/game.rb
@@ -16,6 +16,16 @@ class Game < ApplicationRecord
     end
   end
 
+  def youtube_embed(youtube_url)
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+  end
+
   PLATFORMS = {
     'PS4': 'ps4',
     'XBOX ONE': 'xbox_one',
